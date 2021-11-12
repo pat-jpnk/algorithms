d3a2a9150ce3a1561b532d35f16356db55567e0e
pat-jpnk <jpnksoftware@protonmail.com>
2021-11-12 17:46:14 -0500

    adding

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -14,21 +14,20 @@ the algorithm once there has been an iteration where no switches were made - mea
 array = [8, 5, 2, 9, 5, 6, 3]
 
 def bubble_sort(array):
-  k = len(array)
-
-  for i in range(0, k-1):
-    flag = True
-    for j in range(0,k-1-i):
-      if(array[j] > array[j+1]):
-        temp = array[j]
-        array[j] = array[j+1]
-        array[j+1] = temp
-        flag = False
-
-    if(flag == True):
-      return array
-
-  return array
-
+	k = len(array)
+	
+	for i in range(0,k-1):
+		flag = True
+		for j in range(0,k-1-i):
+			if(array[j] > array[j+1]):
+				temp = array[j]
+				array[j] = array[j+1]
+				array[j+1] = temp
+				flag = False
+		
+		if(flag == True):
+			return array
+		
+	return array
 
 print(bubble_sort(array))
---
+++ sorting/heapsort.py
@@ -0,0 +1,22 @@
+'''
+
+task:
+
+Implement the heap sort algorithm
+
+
+heapsort is not a 'stable' sorting algorithm
+
+'''
+
+array = [8, 5, 2, 9, 5, 6, 3]
+
+
+
+def heap_sort(array):
+
+  
+  return array
+
+
+print(heap_sort(array))
