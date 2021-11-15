ec078537b9d3f5b09b9afed8a3b7bc05133cd451
pat-jpnk <jpnksoftware@protonmail.com>
2021-11-15 17:30:16 -0500

    adding to sort

--- sorting/heapsort.py
+++ sorting/heapsort.py
@@ -9,14 +9,22 @@ heapsort is not a 'stable' sorting algorithm
 
 '''
 
+import heapq 
+
+
 array = [8, 5, 2, 9, 5, 6, 3]
 
 
+# using python heapq module
 
-def heap_sort(array):
-
+def heap_sort_one(array):
+  res = []
+  for val in array:
+    heapq.heappush(res, val)
+  res = [heapq.heappop(res) for i in range(len(res))]
+ 
   
-  return array
+  return res
 
 
-print(heap_sort(array))
+print(heap_sort_one(array))
