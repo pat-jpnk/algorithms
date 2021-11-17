84491fe19ec6fe8e975c6c731b1596c78895abe0
pat-jpnk <jpnksoftware@protonmail.com>
2021-11-16 23:00:37 -0500

    heapsort

--- sorting/heapsort.py
+++ sorting/heapsort.py
@@ -5,56 +5,12 @@ task:
 Implement the heap sort algorithm
 
 
-heapsort is not a 'stable' sorting algorithm
+fyi: heapsort is not a 'stable' sorting algorithm
 
 '''
 
 
-'''
-import heapq 
-
-
-array = [8, 5, 2, 9, 5, 6, 3]
-
-
-# using python heapq module
-
-
-def heap_sort_one(array):
-  res = []
-  for val in array:
-    heapq.heappush(res, val)
-  res = [heapq.heappop(res) for i in range(len(res))]
- 
-  
-  return res
-'''
-
-
-# range(start, stop, step)
-
-
-'''
-
-       (0)
-     /    \
-   (1)    (2)
-  /  \    /  \
- (3) (4) (5) (6)
-
-
-Treap key structure
-
-in array: [0,1,2,3,4,5,6]
-
-'''
-
-data = [4,6,12,53,1,3]
-data5 = [4,6,12,53,1]
-
-data2 = [2,1]
-data3 = [63,23,51,341,32,11]
-data4 = [4,35,231,12,33,2,24,21,6,3,8,3,93,1,92,19,15,13,7,13,6,21,17]
+# class to construct max_heap
 
 class heaps:
   def __init__(self):
@@ -76,7 +32,6 @@ class heaps:
       return
     else:
       p_i = self.parent_index(index)
-      #print("p_i _ index: ", p_i, index)
       if(self.data[p_i] < self.data[index]):
         self.data[p_i], self.data[index] = self.data[index], self.data[p_i]
         self.perculate(p_i)
@@ -84,78 +39,44 @@ class heaps:
   def parent_index(self,i):
     return ((i - 1) // 2)
 
-jj = heaps()
 
+
+# heapsort algorithm
+
+def heap_sort(heap):
+  end = (len(heap) - 1)
+  
+  while(end > 0):
+    heap[end], heap[0] = heap[0], heap[end]
+    end -= 1
+    sift_down(heap,0,end)
+
+def sift_down(heap, start, end):
+  root = start 
+
+  while(((2 * start) + 1) <= end):
+    child = ((2 * start) + 1)
+    swap = root 
+
+    if(heap[swap] < heap[child]):
+      swap = child
+
+    if(((child + 1) <= end) and (heap[swap] < heap[child + 1])):
+      swap = (child + 1)
+     
+    if(swap == root):
+      return
+
+    else:
+      heap[root], heap[swap] = heap[swap], heap[root]
+      swap = root
+
+
+data = [4,6,12,53,1,3]
 kk = heaps()
-
-#print(jj.max_heap(data))
-#print(kk.max_heap(data5))
-#print(jj.max_heap(data2))
-#print(jj.max_heap(data3))
-#print(jj.max_heap(data4))
+jj = kk.max_heap(data)
 
 
-
-
-
-'''
-def heap_sort(data):
-  k = heaps()
-    j = k.max_heap(data)
-  end_index = (len(j) - 1)
-
-  while(end_index > 1):
-    j[0], j[end_index] = j[end_index], j[0]
-    end_index -= 1
-#    print(len(j) - 1)
-#    print(end_index)
-#    print('\n')
-    t = 0
-    while((t <= len(j) - 2) and (j[t] < j[t+1])):
-      print("t: ", t)
-      j[t], j[t+1] = j[t+1], j[t]
-      t += 1
-  
-  return j
-'''
-
-
-def heap_sort(data):
-  k = heaps()
-  j = k.max_heap(data)
-  
-  #print(j)
-
-  focus_index = 0
-  insert_index = (len(j))
-
-  while(insert_index > 1):
-   # print(j)
-   # print("ii: ", insert_index)
-   # print("fi:", focus_index)
-    l_c = (focus_index * 2) + 1
-    r_c = (focus_index * 2) + 2 
-    
-    if(j[l_c] > j[focus_index]):
-      #print("!!: ", j[l_c], j[focus_index]) 
-      #print("II: ", insert_index)
-      focus_index = l_c
-    if(j[r_c] > j[focus_index]):
-      focus_index = r_c
-    
-    #print("ii: ", insert_index)
-    insert_index -= 1
-    if(j[focus_index] > j[insert_index] and (focus_index < insert_index)):
-      print(j)
-      print("j_fi > j_ii: ",j[focus_index],j[insert_index])
-      print("fi ii", focus_index, insert_index)
-      print("\n")
-      j.insert(insert_index, j.pop(focus_index))
-
-    #print(j)
-    #exit()
-  
-  return j
-
-
-print(heap_sort(data))
+print(jj)
+heap_sort(jj)
+print(" -> ",jj)
